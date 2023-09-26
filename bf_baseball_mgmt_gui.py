dff606ee3bbdcde893637f94c769d4500fd07038
brycefish <bryce.k.fish@gmail.com>
2023-09-26 16:38:02 -0600

    start of ch06- adds menu options 1-3

--- bf_baseball_mgmt_gui.py
+++ bf_baseball_mgmt_gui.py
@@ -2,65 +2,145 @@
 # Bryce Fish
 # Project 1
 # Server-Side Programming
+# TO-DO: options-   #1   #4    #5  #6  
 
-def menu_title():
+#####   display menu    #####
+def display_menu():
+    print("================================================================")
+    print("MENU OPTIONS")
+    print("1 - Display lineup" +
+          "\n2 - Add player" +
+          "\n3 - Remove player" +
+          "\n4 - Move player" +
+          "\n5 - Edit player position" +
+          "\n6 - Edit player stats" +
+          "\n7 - Exit program")
     print()
-
-def menu_line():
+    print("POSTIONS")
+    print("C, 1B, 2B, 3B, SS, LF, CF, RF, P")
     print("================================================================")
 
-def menu_options():
-   print("MENU OPTIONS")
-   print("1 – Calculate batting average")
-   print("2 - Exit program")
 
+#####    option 1:    #####
+def display_lineup(lineup):
+    print(f"\tPLAYER\t\tPOS\tAB\tH\tAVG")
+    print("----------------------------------------------------------------")
+    print("\t >>>>this is a placeholder func<<<<")
+    print(lineup)
+
+
+#####    option 2   #####
+def enter_new_player():
+    player = []
+    possible_positions = ("C", "1B", "2B", "3B", "SS", "LF", "CF", "RF", "P")
+    
+    name = input("Name: ")
+    player.append(name)
+
+    while True:
+        position = input("Position: ").upper()
+
+        if position in possible_positions:
+            player.append(position)
+            break
+        else:
+            print("Invalid Position. Please try again")
+            print("POSITIONS")
+            print(possible_positions)
+
+    while True:
+        at_bats = int(input("At bats: "))
+        if at_bats < 0:
+            print("At bats must be a positive integer ")
+        else:
+            break
+    while True:
+        num_hits = int(input("Hits: "))
+        if num_hits < 0:
+            print("Hits must be greater than zero")
+        elif num_hits > at_bats:
+            print("Hits must be lower than at bats")
+        else:
+            break
+    
+    bat_avg = calc_bat_avg(at_bats, num_hits)
+    player.append(at_bats)
+    player.append(num_hits)
+    player.append(bat_avg)
+    print(f"{name} was added")
+
+    return player
+
+
+#####   option 3   #####
+def remove_player(current_lineup):
+
+    if len(current_lineup) < 1 :
+        print("There are no players currently in the lineup")
+
+    while len(current_lineup) > 0:
+        index = int(input("Select lineup number to remove: "))
+        if index < 1 or index > len(current_lineup):
+            print("\nInvalid lineup number. Try again.")
+        else:
+            ################## change vars #####################################
+            player_selected = current_lineup.pop(index - 1)
+            print(f"\n{player_selected[0]} was removed\n")
+            break
+
+    return current_lineup
+
+
+#####   Calculate Batting Average   ####
 def calc_bat_avg(at_bats, num_hits):
-    batting_avg = at_bats / num_hits
+    batting_avg =  num_hits / at_bats
     batting_avg = round(batting_avg, 3)
+    
     return batting_avg
 
-#### Main ####
-def main():
-    menu_line()
-    menu_title()
-    menu_options()
-    menu_line()
 
-    exitChar = True
-    while exitChar == True:
+####    Main    ####
+def main():
+    lineup = []
+    possible_positions = ("C", "1B", "2B", "3B", "SS", "LF", "CF", "RF", "P")
+    #fake lineup for testing of course
+    """test_lineup = [["jonh", "P", 10, 2, 0.2],
+                  ["billy", "SS", 11, 4, 0.364],
+                  ["suzie", "3B", 9, 3, 0.333],
+                  ["roger", "C", 4, 1, 0.25],
+                  ["christopher", "3B", 10, 5, 0.5],
+                  ["randy", "1B", 9, 3, 0.333]]"""
+
+    display_menu()
+    while True:
         print()
         option = input("Menu option: ")
-
-        # added code for logical errors, no error handling
         if option == '1':
-            while True:
-                print("-- Calculate batting average --")
-
-                at_bats = int(input("Official number of at bats: "))
-                if at_bats < 0:
-                    print("Number of at bats must be greater than 0.")
-                    continue
-
-                num_hits = int(input("Number of hits: "))
-                if num_hits < 0:
-                    print("Number of hits must be greater than 0.")
-                    continue
-                elif num_hits > at_bats:
-                    print("Number of hits must be less that number of at bats")
-                    continue
-                
-                batting_average = calc_bat_avg(at_bats, num_hits)
-                print(f"Batting average: {batting_average}")
-                break
+            display_lineup(lineup)
 
         elif option == '2':
-            print("Bye!")
-            exitChar = False
+            player = enter_new_player()
+            lineup.append(player)
+
+        elif option == '3':
+            remove_player(lineup)
+
+        elif option == '4':
+            print("option 4")
+
+        elif option == '5':
+            print("option 5")
+
+        elif option == '6':
+            print("option 6")
+
+        elif option == '7':
+            print("\nBye!")
+            break
 
         else:
             print("Not a valid option. Please try again")
-            print()
-            menu_options()
+
 
 if __name__ == "__main__":
     main()
