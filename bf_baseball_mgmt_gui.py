6dbb868c8ac54706d57685a36b19d8e6003770d3
brycefish <bryce.k.fish@gmail.com>
2023-09-27 20:33:16 -0600

    end of ch06

--- bf_baseball_mgmt_gui.py
+++ bf_baseball_mgmt_gui.py
@@ -2,10 +2,10 @@
 # Bryce Fish
 # Project 1
 # Server-Side Programming
-# TO-DO: options-          #5     #6  
 
-#####   display menu    #####
+####   display menu:    ####
 def display_menu():
+    print()
     print("================================================================")
     print("MENU OPTIONS")
     print("1 - Display lineup" +
@@ -21,14 +21,14 @@ def display_menu():
     print("================================================================")
 
 
-#####    option 1:    #####
+####    option 1: display lineup   ####
 def display_lineup(lineup):
-    print(f"\tPLAYER\t\tPOS\tAB\tH\tAVG")
-    print("----------------------------------------------------------------")
 
     if len(lineup) == 0:
-        print("There are no players on the lineup")
+        print("There are no players to display")
     else:
+        print(f"\tPLAYER\t\tPOS\tAB\tH\tAVG")
+        print("----------------------------------------------------------------")
         print()
         lineup_count = 0
         for player in lineup:
@@ -44,25 +44,21 @@ def display_lineup(lineup):
             print()
 
 
-
-#####    option 2   #####
-def enter_new_player():
+####    option 2: enter new player    ####
+def enter_new_player(valid_positions):
     player = []
-    possible_positions = ("C", "1B", "2B", "3B", "SS", "LF", "CF", "RF", "P")
-    
     name = input("Name: ")
     player.append(name)
 
     while True:
         position = input("Position: ").upper()
-
-        if position in possible_positions:
+        if position in valid_positions:
             player.append(position)
             break
         else:
             print("Invalid Position. Please try again")
-            print("POSITIONS")
-            print(possible_positions)
+            print("POSITIONS:")
+            print(valid_positions)
 
     while True:
         at_bats = int(input("At bats: "))
@@ -88,11 +84,12 @@ def enter_new_player():
     return player
 
 
-#####   option 3   #####
+####   option 3: remove player   ####
 def remove_player(current_lineup):
 
-    if len(current_lineup) < 1 :
-        print("There are no players currently in the lineup")
+    if len(current_lineup) == 0:
+        print("There are no players to display")
+        return
 
     while len(current_lineup) > 0:
         index = int(input("Select lineup number to remove: "))
@@ -100,14 +97,16 @@ def remove_player(current_lineup):
             print("\nInvalid lineup number. Try again.")
         else:
             player_selected = current_lineup.pop(index - 1)
-            print(f"\n{player_selected[0]} was removed\n")
+            print(f"{player_selected[0]} was removed.")
             break
 
-    return current_lineup
 
-
-#####    Option 4: Move Player  #####
+####    Option 4: move player  ####
 def move_player(lineup):
+    if len(lineup) == 0:
+        print("There are no players to display")
+        return
+    
     current_num = int(input("Enter players current lineup number: "))
     if current_num < 1 or current_num > len(lineup):
         print(f"Invalid lineup number, please enter a number between 1 and {len(lineup)}")
@@ -120,11 +119,81 @@ def move_player(lineup):
         print(f"Invalid lineup number, please enter a number between 1 and {len(lineup)}")
     else:
         lineup.insert(new_num - 1, player)
-        print(f"{player[0]} was moved/")
+        print(f"{player[0]} was moved.")
+    
+
+####   option 5: edit player position ####
+def edit_position(lineup, positions):
+    if len(lineup) == 0:
+        print("There are no players to display")
+        return
+
+    print("Editing player position-")
+    while True:
+        player_index = int(input("Enter lineup number: "))
+        if player_index < 1 or player_index > len(lineup):
+            print(f"Invalid lineup number, please enter a number between 1 and {len(lineup)}")
+        else:
+            player = lineup[player_index - 1]
+            print(f"{player[0]}'s current position is {player.pop(1)}")
+            break
+
+    while True:
+        position = input("New position: ").upper()
+        if position in positions:
+            player.insert(1, position)
+            print(f"{player[0]}'s position was changed.")
+            break
+        else:
+            print("\nInvalid position. Please try again")
+            print("POSITIONS")
+            print(positions)
 
 
+####   option 6: edit player stats ####
+def edit_player_stats(lineup):
+    if len(lineup) == 0:
+        print("There are no players to display")
+        return
+    
+    print("Editing player stats-")
+    while True:
+        player_index = int(input("Enter player lineup number: "))
+        if len(lineup) == 0:
+            print("There are no players to display")
+        elif player_index < 1 or player_index > len(lineup):
+            print(f"Invalid lineup number, please enter a number between 1 and {len(lineup)}")
+        else:
+            player = lineup.pop(player_index - 1)
+            print(f"You selected:\t {player[0]} AB={player.pop(2)} H={player.pop(3)}")
+            player.pop()
+            break
 
-#####   Calculate Batting Average   ####
+    while True:
+        at_bats = int(input("At bats: "))
+        if at_bats < 0:
+            print("At bats must be a positive integer ")
+        else:
+            player.append(at_bats)
+            break
+
+    while True:
+        num_hits = int(input("Hits: "))
+        if num_hits < 0:
+            print("Hits must be greater than zero")
+        elif num_hits > at_bats:
+            print("Hits must be lower than at bats")
+        else:
+            player.append(num_hits)
+            break
+    
+    bat_avg = calc_bat_avg(at_bats, num_hits)
+    player.append(bat_avg)
+    lineup.insert(player_index - 1, player)
+    print(f"{player[0]} was added.")
+
+
+####   Calculate Batting Average   ####
 def calc_bat_avg(at_bats, num_hits):
     batting_avg =  num_hits / at_bats
     batting_avg = round(batting_avg, 3)
@@ -134,46 +203,40 @@ def calc_bat_avg(at_bats, num_hits):
 
 ####    Main    ####
 def main():
+    valid_positions = ("C", "1B", "2B", "3B", "SS", "LF", "CF", "RF", "P")
     lineup = []
-    possible_positions = ("C", "1B", "2B", "3B", "SS", "LF", "CF", "RF", "P")
-    #fake lineup for testing of course
-    test_lineup = [["jonh", "P", 10, 2, 0.2],
-                  ["billy", "SS", 11, 4, 0.364],
-                  ["suzie", "3B", 9, 3, 0.333],
-                  ["roger", "C", 4, 1, 0.25],
-                  ["christopher", "3B", 10, 5, 0.5],
-                  ["randy", "1B", 9, 3, 0.333]]
 
     display_menu()
     while True:
         print()
         option = input("Menu option: ")
+
         if option == '1':
-            display_lineup(test_lineup)
+            display_lineup(lineup)
 
         elif option == '2':
-            player = enter_new_player()
+            player = enter_new_player(valid_positions)
             lineup.append(player)
 
         elif option == '3':
-            remove_player(test_lineup)
+            remove_player(lineup)
 
         elif option == '4':
-            move_player(test_lineup)
+            move_player(lineup)
 
         elif option == '5':
-            print("option 5")
+            edit_position(lineup, valid_positions)
 
         elif option == '6':
-            print("option 6")
+            edit_player_stats(lineup)
 
         elif option == '7':
-            print("\nBye!")
             break
-
+        
         else:
             print("Not a valid option. Please try again")
 
+    print("\nBye!")
 
 if __name__ == "__main__":
     main()
