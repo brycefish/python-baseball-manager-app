06b41fc4cec20da9f20ada61411fb44734ab6343
brycefish <bryce.k.fish@gmail.com>
2023-09-30 21:37:40 -0600

    adds csv and py file for testing

--- test_lineup.py
+++ test_lineup.py
@@ -1,6 +1,26 @@
-test_lineup = [["john", "P", 10, 2, 0.2],
-                ["billy", "SS", 11, 4, 0.364],
-                ["suzie", "3B", 9, 3, 0.333],
-                ["roger", "C", 4, 1, 0.25],
-                ["christopher", "3B", 10, 5, 0.5],
-                ["randy", "1B", 9, 3, 0.333]]
+test_lineup =  [["John", "P", 10, 2, 0.2],
+                ["Billy", "SS", 11, 4, 0.364],
+                ["Suzie", "3B", 9, 3, 0.333],
+                ["Roger", "C", 4, 1, 0.25],
+                ["Christopher", "3B", 10, 5, 0.5],
+                ["Randy", "1B", 9, 3, 0.333]]
+
+"""
+John,P,10,2,0.2
+Billy,SS,11,4,0.364
+Suzie,3B,9,3,0.333
+Roger,C,4,1,0.25
+Christopher,3B,10,5,0.5
+Randy,1B,9,3,0.333
+"""
+
+
+test2_lineup = [["Liz", "P", 100, 20, 0.2],
+                ["Franco", "SS", 11, 4, 0.364],
+                ["Gabbie", "3B", 11, 4, 0.362],]
+
+"""
+Liz,P,100,20,0.2
+Franco,SS,11,4,0.364
+Gabbie,3B,11,4,0.362
+"""
